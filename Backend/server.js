988a507810f8538f020ac50fc6cf2b121c1f00d4
AmanSangani <127185092+AmanSangani@users.noreply.github.com>
2023-12-27 18:57:44 +0530

    change in varcel

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,11 @@ connectDB();
 
 const router = require('./router')
 app.use('/auth', router)
+app.get('/', (req, res) =>{
+    res.status(200).json({
+        message: 'Welcome to the'
+    })
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
