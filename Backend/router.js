6b6907fc00dc14649a975ce8f51e18aa52d9bba2
AmanSangani <127185092+AmanSangani@users.noreply.github.com>
2024-02-07 22:56:22 +0530

    c

--- Backend/router.js
+++ Backend/router.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '../eatify-main/public/imgs/');
+    cb(null, 'Backend/uploads/');
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
