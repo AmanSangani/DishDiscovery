fbf2bca980ed4e3c9e7ae90a6964a2d2b7ba7c9d
AmanSangani <127185092+AmanSangani@users.noreply.github.com>
2024-02-07 22:00:48 +0530

    commit for deploying

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-        try {
-                const conn = await mongoose.connect(`mongodb://0.0.0.0:27017/aman`, {
-                        useNewUrlParser: true,
-                        useUnifiedTopology: true
-                });
-                console.log(`MongoDB Connected: ${conn.connection.host}`);
-        } catch (error) {
-                console.error(error);
-                process.exit(1);
-        }
-}
+  try {
+    const conn = await mongoose.connect(
+      `mongodb+srv://aman:aman@cluster0.ez04noz.mongodb.net/dishdiscovery`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const connectDB = require('./connection');
 const cors = require('cors');
+const path = require('path');
 
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+    if (req.originalUrl.includes("/undefined")) {
+      req.url = req.originalUrl.replace("/undefined", "");
+      req.originalUrl = req.url;
+    }
+    if (req.originalUrl.includes("//")) {
+      req.url = req.originalUrl.replace("//", "/");
+      req.originalUrl = req.url;
+    }
+    next();
+  });
 
 app.use(cors());
 
@@ -17,6 +29,15 @@ app.get('/', (req, res) =>{
         message: 'Welcome to the'
     })
 })
+
+//------------Deployment-------------
+const __dirname1 = path.resolve();
+app.use(express.static(path.join(__dirname1, "/Frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+
+//------------Deployment-------------
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
